a85f442e22ea465d2a7e5a7e8c76c4701cd5c1a6
vinodhm <vinodh.manda@gmail.com>
2018-10-11 08:20:22 -0400

    added Swagger

---
+++ src/test/java/com/vin/geomap/services/GeoMapImplTest.java
@@ -0,0 +1,41 @@
+package com.vin.geomap.services;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeoMapImplTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    GeoMap geoMap;
+
+    @Test
+    public void toCheckConnectionInSameTree() {
+        Assert.assertEquals(true,geoMap.checkConnectivityBetweenCities("Hoboken","north Bergen"));
+    }
+
+    @Test
+    public void toCheckConnectionCitiesNotInSameTree() {
+        Assert.assertEquals(false,geoMap.checkConnectivityBetweenCities("Hoboken","newark"));
+    }
+
+    @Test
+    public void toCheckifOppositeDirectionConnectionExist() {
+        Assert.assertEquals(true,geoMap.checkConnectivityBetweenCities("north Bergen","Hoboken"));
+    }
+
+    @Test
+    public void toCheckifSourceAndDestinationAreSame() {
+        Assert.assertEquals(true,geoMap.checkConnectivityBetweenCities("Hoboken","Hoboken"));
+    }
+
+}
